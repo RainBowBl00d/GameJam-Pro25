e752073c75432999a01847978c3065d5c08095a3
VirtuousDemon <lilleras@ut.ee>
2025-01-25 03:19:24 +0200

    Scenetest

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -4,6 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class Load : MonoBehaviour
 {
+    public void LoadScene(Scene scene)
+    {
+        SceneManager.LoadSceneAsync(scene.name);
+    }
     public void LoadScene(int scene)
     {
         SceneManager.LoadSceneAsync(scene);
