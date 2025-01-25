bcfa8cfdf4d684322b6bea4f4c58deef647ec63f
VirtuousDemon <lilleras@ut.ee>
2025-01-25 02:15:02 +0200

    Load scene by name

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -1,9 +1,15 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Load : MonoBehaviour
 {
     public void LoadScene(int scene)
+    {
+        SceneManager.LoadSceneAsync(scene);
+    }
+
+    public void LoadScene(String scene)
     {
         SceneManager.LoadSceneAsync(scene);
     }
