6d6bf456f67620fa8cfa1c9777abc04897b98ba9
RainBowBl00d <Innarraudsepp@gmail.com>
2025-01-26 13:16:13 +0200

    Commit

--- Assets/Scripts/MiniGames/Key Notes/KeyNotesGameHelper.cs
+++ Assets/Scripts/MiniGames/Key Notes/KeyNotesGameHelper.cs
@@ -1,21 +1,25 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class KeyNotesGameHelper : MonoBehaviour
 {
+    public static event Action<int> OnLevelChanged; // Event to notify level changes
+
     [SerializeField] KeyNotesGame keyNoteGame;
     [SerializeField] GameObject button, ready, ani, tutorial;
     [SerializeField] private ProgressSlider slider;
     [SerializeField] Animator animator;
 
-    [SerializeField]KeyNoteGameLevelStats levelStats1, levelStats2, levelStats3;
+    [SerializeField] KeyNoteGameLevelStats levelStats1, levelStats2, levelStats3;
 
     public void Ready()
     {
         StartCoroutine(RoundManage());
     }
-    IEnumerator RoundManage() 
+
+    IEnumerator RoundManage()
     {
         ResetValues(levelStats3);
         ResetValues(levelStats2);
@@ -23,6 +27,7 @@ public class KeyNotesGameHelper : MonoBehaviour
 
         ani.SetActive(true);
         animator.Play("countDownAnim");
+        NotifyLevelChanged(0);
         yield return new WaitForSecondsRealtime(4f);
         ani.SetActive(false);
         Level1();
@@ -31,6 +36,7 @@ public class KeyNotesGameHelper : MonoBehaviour
         {
             ani.SetActive(true);
             animator.Play("countDownAnim");
+            NotifyLevelChanged(1);
             yield return new WaitForSecondsRealtime(4f);
             ani.SetActive(false);
             Level2();
@@ -38,6 +44,8 @@ public class KeyNotesGameHelper : MonoBehaviour
         }
         else
         {
+            
+            NotifyLevelChanged(-1);
             ready.SetActive(true);
             tutorial.SetActive(true);
         }
@@ -45,20 +53,29 @@ public class KeyNotesGameHelper : MonoBehaviour
         {
             ani.SetActive(true);
             animator.Play("countDownAnim");
+            NotifyLevelChanged(2);
             yield return new WaitForSecondsRealtime(4f);
             ani.SetActive(false);
             Level3();
             while (levelStats3.Running) yield return new WaitForSecondsRealtime(1f);
         }
-        else
-        {
-            ready.SetActive(true);
-            tutorial.SetActive(true);
-        }
+        
+        NotifyLevelChanged(-1);
+        ready.SetActive(true);
+        tutorial.SetActive(true);
+        
         if (levelStats3.Completed)
         {
             button.SetActive(true);
         }
+    }
+    public void Next()
+    {
+        SceneManager.LoadScene("Stage 2");
+    }
+    private void NotifyLevelChanged(int level)
+    {
+        OnLevelChanged?.Invoke(level); // Trigger event
     }
 
     void Level1()
@@ -68,7 +85,7 @@ public class KeyNotesGameHelper : MonoBehaviour
     }
     void Level2()
     {
-        if (!levelStats1.Completed )
+        if (!levelStats1.Completed)
         {
             Debug.Log("Level 1 unCompleted");
             return;
@@ -78,7 +95,7 @@ public class KeyNotesGameHelper : MonoBehaviour
     }
     void Level3()
     {
-        if (!levelStats2.Completed )
+        if (!levelStats2.Completed)
         {
             Debug.Log("Level 2 unCompleted");
             return;
@@ -87,10 +104,7 @@ public class KeyNotesGameHelper : MonoBehaviour
         slider.StartGame(levelStats3);
         if (levelStats3.Completed) button.SetActive(true);
     }
-    public void Next()
-    {
-        SceneManager.LoadScene("Stage 2");
-    }
+
     void ResetValues(KeyNoteGameLevelStats stats)
     {
         stats.Correct = 0;
@@ -99,6 +113,8 @@ public class KeyNotesGameHelper : MonoBehaviour
         stats.Completed = false;
     }
 }
+
+
 [System.Serializable]
 public class KeyNoteGameLevelStats
 {
@@ -107,11 +123,10 @@ public class KeyNoteGameLevelStats
     public bool ascending;
     public float timeToWaitBtwRealses;
     public float hitFactorRequirement;
-    
 
     public bool Completed { get; set; }
     public int Lost { get; set; }
     public int Missed { get; set; }
-    public int Correct {get; set;}
+    public int Correct { get; set; }
     public bool Running { get; set; }
 }
